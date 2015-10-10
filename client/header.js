b594aa973da1ba161133b836518921797baf1c4b
Luke Baker <luke@gravitywell.co.uk>
2015-10-10 23:33:01 +0100

    Added dynamic player listings. Your player is first in the list with the correct colour for the player

--- client/header.js
+++ client/header.js
@@ -3,31 +3,43 @@ Template.header.helpers({
     return 'selected';
   },
   getUserList: function() {
-    var users = [];
+    var availableUsers = [];
+    var user = Session.get('user');
+    var userColor = humanColour( user.color );
 
-    users = [
-        {
-            name: 'Projectmoon',
-            colour: 'cyan'
-        },
-        {
-            name: 'Mighty Quinn',
-            colour: 'purple'
-        },
-        {
-            name: 'Stackpool',
-            colour: 'yellow'
-        },
-        {
-            name: 'Bakeruk',
-            colour: 'teal'
-        },
-        {
-            name: 'Donald',
-            colour: 'coral'
-        }
+    availableUsers = [
+      {
+        name: 'Projectmoon',
+        colour: 'cyan'
+      },
+      {
+        name: 'Mighty Quinn',
+        colour: 'purple'
+      },
+      {
+        name: 'Stackpool',
+        colour: 'yellow'
+      },
+      {
+        name: 'Bakeruk',
+        colour: 'teal'
+      },
+      {
+        name: 'Donald',
+        colour: 'coral'
+      }
     ];
 
-    return users;
+    for (var i in availableUsers) {
+
+      // Make the user first in our list
+      if (userColor == availableUsers[i].colour) {
+
+        // Move the user to the top of the list so that they are first
+        availableUsers.splice(0, 0, availableUsers.splice(i, 1)[0]);
+      }
+    }
+
+    return availableUsers;
   }
 });
