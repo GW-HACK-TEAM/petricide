ff79f024c81e02558a29c76f338ee6944096fa88
Luke Baker <luke@gravitywell.co.uk>
2015-10-11 00:00:16 +0100

    Fixed bug on undefined variable of load

--- client/header.js
+++ client/header.js
@@ -1,11 +1,8 @@
 Template.header.helpers({
-  active: function(classname) {
-    return 'selected';
-  },
   getUserList: function() {
     var availableUsers = [];
     var user = Session.get('user');
-    var userColor = humanColour( user.color );
+    var userColor = '';
 
     availableUsers = [
       {
@@ -30,13 +27,22 @@ Template.header.helpers({
       }
     ];
 
-    for (var i in availableUsers) {
+    // Check if the user object has a colour property
+    if (
+      user instanceof Object &&
+      user.hasOwnProperty('color')
+    ) {
+      userColor = humanColour( user.color );
 
-      // Make the user first in our list
-      if (userColor == availableUsers[i].colour) {
+      // Find the users colour and make them first on the list
+      for (var i in availableUsers) {
 
-        // Move the user to the top of the list so that they are first
-        availableUsers.splice(0, 0, availableUsers.splice(i, 1)[0]);
+        // Make the user first in our list
+        if (userColor == availableUsers[i].colour) {
+
+          // Move the user to the top of the list so that they are first
+          availableUsers.splice(0, 0, availableUsers.splice(i, 1)[0]);
+        }
       }
     }
 
