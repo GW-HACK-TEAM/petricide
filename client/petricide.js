78256839ff35cafc76a0926f6bce821bed4c2250
Chris Quinn <chris@gravitywell.co.uk>
2015-10-10 14:40:01 +0100

    removed all the starter code

--- client/petricide.js
+++ client/petricide.js
@@ -1,15 +1,5 @@
-// counter starts at 0
-Session.setDefault('counter', 0);
-
-Template.hello.helpers({
-  counter: function () {
-    return Session.get('counter');
-  }
+Template.app.helpers({
 });
 
-Template.hello.events({
-  'click button': function () {
-    // increment the counter when button is clicked
-    Session.set('counter', Session.get('counter') + 1);
-  }
+Template.app.events({
 });
