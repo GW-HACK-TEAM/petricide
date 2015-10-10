a51135dd471bc641b1ac863de0835b7ba705ab5c
Chris Quinn <chris@gravitywell.co.uk>
2015-10-10 17:16:25 +0100

    basic user id assign and payload trafficing

--- client/petricide.js
+++ client/petricide.js
@@ -1,37 +1,56 @@
-example = new ReactiveVar();
+var payload = [];
+var payloadDep = new Tracker.Dependency;
+
+var getPayload = function () {
+  payloadDep.depend();
+  return payload;
+};
+var addToPayload = function (arr) {
+  payload.push(arr);
+  payloadDep.changed();
+};
+
+var user = false;
+var userDep = new Tracker.Dependency;
+
+var getUser = function () {
+  userDep.depend();
+  return user;
+};
+var setUser = function (dets) {
+  user = dets;
+  userDep.changed();
+};
 
 Template.app.helpers({
-  getGameData:function(){
-    return _.first(ReactiveMethod.call('testMethod', example.get()));
+  getGameData: function () {
+    return _.first(ReactiveMethod.call('testMethod', getPayload()));
   },
-  user:function(){
-
-    // if new user
-    if(!Session.get('userId')){
-      Session.set('userId', ReactiveMethod.call('newuser');
+  user: function () {
+    var response;
+    if (!getUser()) {
+      response = ReactiveMethod.call('newUser');
+      if (response && response.validPlayer) {
+        setUser(response);
+      }
+    } else {
+      return getUser();
     }
+  },
+  getUserDets:function(){
+    return getUser();
   }
-
 });
 
 Template.app.events({
-  'click #canvas':function(){
-    Session.set('trigger', Date.now());
-    example.set([Session.get('userId'), coords]);
+  'click #canvas': function (e) {
+
+    var click = [33, 44];
+    addToPayload(click);
+    console.log(getPayload());
   }
 });
-
-Template.app.onRendered(function(){
-
-
-
-
-
-
-
-
-
-
+Template.app.onRendered(function () {
 
   var canvas = document.getElementById("canvas");
   var ctx = canvas.getContext("2d");
@@ -53,21 +72,18 @@ Template.app.onRendered(function(){
 
   var playerColor = 'red';
 
-  function mode(array)
-  {
-    if(array.length === 0)
+  function mode(array) {
+    if (array.length === 0)
       return null;
     var modeMap = {};
     var maxEl = array[0], maxCount = 1;
-    for(var i = 0; i < array.length; i++)
-    {
+    for (var i = 0; i < array.length; i++) {
       var el = array[i];
-      if(modeMap[el] === null)
+      if (modeMap[el] === null)
         modeMap[el] = 1;
       else
         modeMap[el]++;
-      if(modeMap[el] > maxCount)
-      {
+      if (modeMap[el] > maxCount) {
         maxEl = el;
         maxCount = modeMap[el];
       }
@@ -87,103 +103,103 @@ Template.app.onRendered(function(){
     node.color = 'black';
     node.active = false;
     node.opacity = 1;
-    node.draw = function(color) {
+    node.draw = function (color) {
       ctx.globalAlpha = node.opacity;
       ctx.fillStyle = color;
-      ctx.fillRect(node.lon*size, node.lat*size, size, size);
+      ctx.fillRect(node.lon * size, node.lat * size, size, size);
     };
-    node.activate = function(color) {
+    node.activate = function (color) {
       node.color = color;
       node.active = true;
       node.changeHealth(15);
       node.draw(color);
     };
-    node.changeHealth = function(val) {
+    node.changeHealth = function (val) {
       node.health = node.health + val;
-      if ( node.health > 100 ) {
+      if (node.health > 100) {
         node.health = 100;
       }
-      if ( node.health < 0 ) {
+      if (node.health < 0) {
         node.health = 0;
       }
 
-      node.opacity = node.health/100;
+      node.opacity = node.health / 100;
       node.draw(node.color);
 
-      if ( node.health === 0 ) {
+      if (node.health === 0) {
         node.kill();
       }
     };
-    node.kill = function() {
+    node.kill = function () {
       node.active = false;
       node.inactiveCount = 5;
-      node.neighbours().forEach(function(item) {
-        if ( item.active && item.color !== node.color ) {
+      node.neighbours().forEach(function (item) {
+        if (item.active && item.color !== node.color) {
           item.changeHealth(-50);
         }
       });
     };
     node.inactiveCount = 0;
-    node.update = function() {
-      if ( node.inactiveCount > 0 ) {
+    node.update = function () {
+      if (node.inactiveCount > 0) {
         node.inactiveCount--;
         return;
       }
-      activeNeighBours = _.filter(node.neighbours(), function(item) {
+      activeNeighBours = _.filter(node.neighbours(), function (item) {
         return item.active && item.health >= 75;
       });
 
-      if ( !node.active && activeNeighBours.length === 0 ) {
+      if (!node.active && activeNeighBours.length === 0) {
         return;
       }
 
-      rand = Math.round(Math.random()*2) + 4;
+      rand = Math.round(Math.random() * 2) + 4;
 
-      if ( node.active ) {
+      if (node.active) {
 
-        if ( activeNeighBours.length < rand ) {
+        if (activeNeighBours.length < rand) {
           node.changeHealth(1.5);
-        } else if ( activeNeighBours.length >= rand ) {
+        } else if (activeNeighBours.length >= rand) {
           node.changeHealth(-0.75);
         }
       }
 
-      if ( !node.active && activeNeighBours.length >= 3 ) {
+      if (!node.active && activeNeighBours.length >= 3) {
         activeColor = mode(_(activeNeighBours).pluck('color'));
         node.activate(activeColor);
       }
     };
     node.cachedNeighbours = [];
-    node.neighbours = function() {
-      if ( node.cachedNeighbours.length ) {
+    node.neighbours = function () {
+      if (node.cachedNeighbours.length) {
         return node.cachedNeighbours;
       }
       var arr = [];
-      if ( nodes[i - 1] ) {
-        if ( nodes[i - 1][j - 1] ) {
+      if (nodes[i - 1]) {
+        if (nodes[i - 1][j - 1]) {
           arr.push(nodes[i - 1][j - 1]);
         }
-        if ( nodes[i - 1][j] ) {
+        if (nodes[i - 1][j]) {
           arr.push(nodes[i - 1][j]);
         }
-        if ( nodes[i - 1][j + 1] ) {
+        if (nodes[i - 1][j + 1]) {
           arr.push(nodes[i - 1][j + 1]);
         }
       }
-      if ( nodes[i][j - 1] ) {
+      if (nodes[i][j - 1]) {
         arr.push(nodes[i][j - 1]);
       }
-      if ( nodes[i][j + 1] ) {
+      if (nodes[i][j + 1]) {
         arr.push(nodes[i][j + 1]);
       }
-      if ( nodes[i + 1] ) {
-        if ( nodes[i + 1][j - 1] ) {
+      if (nodes[i + 1]) {
+        if (nodes[i + 1][j - 1]) {
           arr.push(nodes[i + 1][j - 1]);
         }
-        if ( nodes[i + 1][j] ) {
+        if (nodes[i + 1][j]) {
           arr.push(nodes[i + 1][j]);
         }
-        if ( nodes[i + 1][j + 1] ) {
+        if (nodes[i + 1][j + 1]) {
           arr.push(nodes[i + 1][j + 1]);
         }
       }
@@ -191,17 +207,17 @@ Template.app.onRendered(function(){
       return arr;
     };
 
-    node.clickEffect = function(myColor) {
-      var  val = 60;
-      if ( node.active && node.color !== myColor ) {
+    node.clickEffect = function (myColor) {
+      var val = 60;
+      if (node.active && node.color !== myColor) {
         node.changeHealth(0 - val);
-      } else if ( !node.active ) {
+      } else if (!node.active) {
         node.activate(myColor);
         node.playerActivated = true;
 
       }
-      node.neighbours().forEach(function(item) {
-        if ( item.active && item.color !== myColor ) {
+      node.neighbours().forEach(function (item) {
+        if (item.active && item.color !== myColor) {
           item.changeHealth(0 - val);
         }
       });
@@ -217,9 +233,9 @@ Template.app.onRendered(function(){
   }
 
 
-  for(i = 0; i < gridWidth/size; i++) {
+  for (i = 0; i < gridWidth / size; i++) {
     nodes.push([]);
-    for(j = 0; j < gridHeight/size; j++) {
+    for (j = 0; j < gridHeight / size; j++) {
       nodes[i].push(createNode(i, j));
     }
   }
@@ -245,8 +261,8 @@ Template.app.onRendered(function(){
 
   function cycle() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for(i = 0; i < nodes.length; i++) {
-      for(j = 0; j < nodes[i].length; j++) {
+    for (i = 0; i < nodes.length; i++) {
+      for (j = 0; j < nodes[i].length; j++) {
         nodes[i][j].update();
       }
     }
@@ -263,29 +279,29 @@ Template.app.onRendered(function(){
     x -= canvas.offsetLeft;
     y -= canvas.offsetTop;
 
-    var contextX = Math.floor(x/size);
-    var contextY = Math.floor(y/size);
+    var contextX = Math.floor(x / size);
+    var contextY = Math.floor(y / size);
 
     nodes[contextX][contextY].clickEffect(playerColor);
   }
 
   var rand1;
   var rand2;
-  setInterval( function aiCycle() {
-    if ( running ) {
-      rand1 = Math.round(Math.random() * gridWidth/size);
-      rand2 = Math.round(Math.random() * gridHeight/size);
+  setInterval(function aiCycle() {
+    if (running) {
+      rand1 = Math.round(Math.random() * gridWidth / size);
+      rand2 = Math.round(Math.random() * gridHeight / size);
       nodes[rand1][rand2].clickEffect('blue');
 
-      rand1 = Math.round(Math.random() * gridWidth/size);
-      rand2 = Math.round(Math.random() * gridHeight/size);
+      rand1 = Math.round(Math.random() * gridWidth / size);
+      rand2 = Math.round(Math.random() * gridHeight / size);
       nodes[rand1][rand2].clickEffect('green');
     }
   }, 1000);
 
-  setInterval(function() {
-    if ( running ) {
+  setInterval(function () {
+    if (running) {
       cycle();
     }
-  }, 1000/60);
+  }, 1000 / 60);
 });
