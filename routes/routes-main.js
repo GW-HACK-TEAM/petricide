4f47a9a69496bad6f6e86ff183502105ef119058
Luke Baker <luke@gravitywell.co.uk>
2015-10-11 01:21:34 +0100

    Added about page

--- routes/routes-main.js
+++ routes/routes-main.js
@@ -3,11 +3,34 @@
  * Routes that are visible to all (public) users.
  */
 
+Router.route('/', {
+  path: '/',
+  name:'Petricide',
+  template: 'app',
+  onBeforeAction: function() {
+    Session.set('currentRoute', 'app');
+    this.next();
+  }
+});
+
+
 Router.route('404', {
   path: '/nope',
   name:'nope',
   template: 'nope',
   onBeforeAction: function() {
+    Session.set('currentRoute', '404');
+    this.next();
+  }
+});
+
+
+Router.route('about', {
+  path: '/about',
+  name:'About',
+  template: 'about',
+  onBeforeAction: function() {
+    Session.set('currentRoute', 'about');
     this.next();
   }
 });
